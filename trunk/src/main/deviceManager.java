5547870c3c10725efb24093f28b0ec0c1f3b381f
dustinsallinone <dustinsallinone@d304352e-c457-11de-ba9a-9db95b2bc6c5>
2009-11-18 21:14:25 +0000

    device manager

---
+++ trunk/src/main/deviceManager.java
@@ -0,0 +1,18 @@
+package main;
+
+public class deviceManager {
+	
+	/*
+	 * schedule a call to device when device is available
+	 */
+	public void callToDevice(int jobNumber, int deviceNumber)
+	{
+	}
+	
+	/*
+	 * update device connections
+	 */
+	public void update()
+	{
+	}
+}
