b8aeb7b1187bc84908d31bba75758443cfc525bf
dustinsallinone <dustinsallinone@d304352e-c457-11de-ba9a-9db95b2bc6c5>
2009-11-18 21:50:16 +0000

    file loader

---
+++ trunk/src/main/fileLoader.java
@@ -0,0 +1,36 @@
+package main;
+
+public class fileLoader {
+	
+	/*
+	 * this returns an array of jobs from the given filenames
+	 */
+	public job[] getJobs(String[] filenames)
+	{
+		return null;
+	}
+	
+	/*
+	 * this returns the job object created from the specified filename
+	 */
+	public job loadJob(String filename)
+	{
+		return null;
+	}
+	
+	/*
+	 * this returns an array of devices from the given filenames
+	 */
+	public device[] getDeviceList(String[] filenames)
+	{
+		return null;
+	}
+	
+	/*
+	 * this returns the device object created from the specified filename
+	 */
+	public device loadDevice(String filename)
+	{
+		return null;
+	}
+}
