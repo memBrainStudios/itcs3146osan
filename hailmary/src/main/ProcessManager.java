71ac6b937174057b3a71940f09b0aeff27a5dedd
mradams8590 <mradams8590@d304352e-c457-11de-ba9a-9db95b2bc6c5>
2009-11-26 01:07:15 +0000

    Adding process manager as well as the pages for the doubly linked list and it's nodes

--- hailmary/src/main/ProcessManager.java
+++ hailmary/src/main/ProcessManager.java
@@ -1,70 +1,62 @@
-package main;
+package main
+ 
 /*
  *      processManager.java
  *      
- * 	
+ *  
  */
 
 
-public class ProcessManager {
-	LinkedList() list = new LinkedList();
-	int pointer = 0;
-	int lastPoint = 0;
-	int timeQuantum = 10;
-	int endCycle;
-	
-	public ProcessManager()
-	{
-		
-		
-	}
-	public void newJob(int activeJob){
-		list.addLast(activeJob);
-		list.size = list.size + 1;
-		lastPoint = list.size - 1;
-	}
-	public void deleteJob(int deleteJob){
-		int jobCounter = 0;
-		if jobCounter < list.size
-			if list.get(jobCounter) =! deleteJob
-				jobCounter= jobCounter + 1;
-			else
-				if jobCounter > pointer
-					
-	}
-	/*
-	 * returns true if the CPU is to continue processing the current job
-	 */
-	public boolean continueCurrentJob(int cpuCycle)
-	{
-		if cpuCycle = endCycle{
-		//determine whether or not to continue processing the current job
-			if pointer < list.size{
-				pointer = pointer + 1;
-				return false;
-			}
-			else{
-				pointer = 0;
-			return false;
-			}
-		
-		else 
-			return true;
-		}
-	}
-	
-	/*
-	 * tells the CPU which job to process next
-	 */
-	public int loadJob()
-	{
-		if list == null
-			return -1;
-		else{
-		return list.get(pointer);
-		endCycle = cpuCycle + timeQuantum;
-		
-		}
-		//returns the next job that will be processed by the cpu
-	}
+public class processManager {
+    List<String> list = new List<String>();
+    int timeQuantum = 10;
+    int endCycle;
+    private int size;
+    
+    public processManager()
+    {
+        size = 0;
+    }
+    public void newJob(int activeJob) throws IndexOutOfBound {
+            list.add(size,Integer.toString(activeJob));
+            size = size++;
+    }
+    public void jobFinished(int deleteJob) throws IndexOutOfBound{
+        int jobCounter = 0;
+        if (jobCounter < size){
+            String holder = list.get(jobCounter);
+            if (Integer.parseInt(holder) == deleteJob){
+                list.remove(jobCounter);
+                size--;
+            }
+            else{
+                jobCounter = jobCounter + 1;
+            }
+        }
+    }
+    /*
+     * returns true if the CPU is to continue processing the current job
+     */
+    public boolean continueCurrentJob(int cpuCycle){
+        if (cpuCycle == endCycle)
+        //determine whether or not to continue processing the current job
+            return false;
+        else 
+            return true;
+    }
+    
+    /*
+     * tells the CPU which job to process next
+     */
+    public int loadJob(int cpuCycle) throws IndexOutOfBound
+    {
+        if( list == null)
+            return -1;
+        else{
+            endCycle = cpuCycle + timeQuantum;
+            return Integer.valueOf(list.get(list.movePointer()));
+        
+        }
+        //returns the next job that will be processed by the cpu
+    }
 }
